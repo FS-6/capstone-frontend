b26fc432caf9d65b7037c33410c038c151c3fd77
alfian <devsroot8@gmail.com>
2023-11-13 22:09:30 +0700

    change title

--- kulineri-frontend/src/App.jsx
+++ kulineri-frontend/src/App.jsx
@@ -1,12 +1,9 @@
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">alfian pernah disini</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
