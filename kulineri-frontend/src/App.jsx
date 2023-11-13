f7194ac63f530140249443a003a10d3681f79e01
Muhammad Zidan <zidanidan11@gmail.com>
2023-11-13 22:26:53 +0700

    change title

--- kulineri-frontend/src/App.jsx
+++ kulineri-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">alfian pernah disini</h1>
+      <h1 className="text-3xl font-bold underline">zidan pernah disini</h1>
     </>
   );
 }
