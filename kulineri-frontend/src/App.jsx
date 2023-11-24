fc6a305ee4eb4042a8d087175bfb29fff904bc85
Muhammad Zidan <zidanidan11@gmail.com>
2023-11-24 22:19:29 +0700

    fix: change route pages

--- kulineri-frontend/src/App.jsx
+++ kulineri-frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Login from "./pages/login";
+import LandingPage from "./pages/landingpage";
+import Register from "./pages/register";
+import Home from "./pages/home";
+import Settings from "./pages/profilesettings";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">zidan pernah disini</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<Home />} />
+        <Route path="profilesettings" element={<Settings />} />
+      </Routes>
+    </Router>
   );
 }
 
